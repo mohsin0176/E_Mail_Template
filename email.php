b700e9ded8aade98ce6d3f884659d7a711cc69a1
mohsin <msnju2010@gmail.com>
2020-12-02 22:57:23 +0600

    they are Email Template code

---
+++ email.php
@@ -0,0 +1,128 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" lang="en-GB">
+<head>
+	<link rel="icon" href="images/favicon.png" type="images/png">
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+	<title>WelCome Email From PNS GROUP</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+
+	<style type="text/css">
+		a[x-apple-data-detectors] {color: inherit !important;}
+	</style>
+
+</head>
+<body style="margin:0; padding:0;border-color:green;">
+	<table role="presentation" border="0" cellpadding="0" cellspacing="0" width="100%">
+		<tr>
+			<td style="padding: 20px 0 30px 0;">
+
+<table align="center" border="0" cellpadding="0" cellspacing="0" width="600" style="border-collapse: collapse; border: 1px solid #cccccc;">
+	<tr>
+		<td align="center" bgcolor="#70bbd9" style="padding: 40px 0 30px 0;">
+			<img src="images/header.png" alt="Creating Email Magic." width="300" height="230" style="display: block;" />
+		</td>
+	</tr>
+	<tr>
+		<td bgcolor="#ffffff" style="padding: 40px 30px 40px 30px;">
+			<table border="0" cellpadding="0" cellspacing="0" width="100%" style="border-collapse: collapse;">
+				<tr>
+					<td style="color: #153643; font-family: Arial, sans-serif;">
+						<h1 style="font-size: 24px; margin: 0;">Email Greetings From PNS GROUP</h1>
+					</td>
+				</tr>
+				<tr>
+					<td style="color: #153643; font-family: Arial, sans-serif; font-size: 16px; line-height: 24px; padding: 20px 0 30px 0;">
+						<p style="margin: 0;text-align: justify">PNS Industrial Machineries Ltd is the leading importer of reconditioned Offset Printing & Packaging Machinery. It is a solution to your everyday demands in an ever increasingly competitive print market.It supplies the printing machinery all over Bangladesh, offering you the opportunity of purchasing high quality, reconditioned press machinery at a vastly reduced rate than the original retail price</p>
+					</td>
+				</tr>
+				<tr>
+					<td>
+						<table border="0" cellpadding="0" cellspacing="0" width="100%" style="border-collapse: collapse;">
+							<tr>
+								<td width="260" valign="top">
+									<table border="0" cellpadding="0" cellspacing="0" width="100%" style="border-collapse: collapse;">
+										<tr>
+											<td>
+												<img src="images/left.png" alt="" width="100%" height="140" style="display: block;" />
+											</td>
+										</tr>
+										<tr>
+											<td style="color: #153643; font-family: Arial, sans-serif; font-size: 16px; line-height: 24px; padding: 25px 0 0 0;">
+												<p style="margin: 0;text-align: justify">PNS Industrial Machineries Ltd is the leading importer of reconditioned Offset Printing & Packaging Machinery. It is a solution to your everyday demands in an ever increasingly competitive print market.It supplies the printing machinery all over Bangladesh, offering you the opportunity of purchasing high quality, reconditioned press machinery at a vastly reduced rate than the original retail price.</p>
+											</td>
+										</tr>
+									</table>
+								</td>
+								<td style="font-size: 0; line-height: 0;" width="20">&nbsp;</td>
+								<td width="260" valign="top">
+									<table border="0" cellpadding="0" cellspacing="0" width="100%" style="border-collapse: collapse;">
+										<tr>
+											<td>
+												<img src="images/right.png" alt="" width="100%" height="140" style="display: block;" />
+											</td>
+										</tr>
+										<tr>
+											<td style="color: #153643; font-family: Arial, sans-serif; font-size: 16px; line-height: 24px; padding: 25px 0 0 0;">
+												<p style="margin: 0;text-align: justify">PNS Industrial Machineries Ltd is the leading importer of reconditioned Offset Printing & Packaging Machinery. It is a solution to your everyday demands in an ever increasingly competitive print market.It supplies the printing machinery all over Bangladesh, offering you the opportunity of purchasing high quality, reconditioned press machinery at a vastly reduced rate than the original retail price.</p>
+											</td>
+										</tr>
+									</table>
+								</td>
+							</tr>
+						</table>
+					</td>
+				</tr>
+			</table>
+		</td>
+	</tr>
+	<tr>
+		<td bgcolor="#ee4c50" style="padding: 30px 30px;">
+    		<table border="0" cellpadding="0" cellspacing="0" width="100%" style="border-collapse: collapse;">
+				<tr>
+
+					<td align="center">
+						<table border="0" cellpadding="0" cellspacing="0" style="border-collapse: collapse;">
+							<tr>
+									<td>
+									<a href="http://www.gmail.com/">
+										<img src="images/gm.png" alt="Gmail." width="38" height="38" style="display: block;" border="0" />
+									</a>
+								</td>
+								<td style="font-size: 0; line-height: 0;" width="20">&nbsp;</td>
+									<td>
+									<a href="http://www.instagram.com/">
+										<img src="images/ins.png" alt="Instagram." width="38" height="38" style="display: block;" border="0" />
+									</a>
+								</td>
+								<td style="font-size: 0; line-height: 0;" width="20">&nbsp;</td>
+									<td>
+									<a href="http://www.skype.com/">
+										<img src="images/sk.png" alt="Skype." width="38" height="38" style="display: block;" border="0" />
+									</a>
+								</td>
+								<td style="font-size: 0; line-height: 0;" width="20">&nbsp;</td>
+								<td>
+									<a href="http://www.twitter.com/">
+										<img src="images/tw.png" alt="Twitter." width="38" height="38" style="display: block;" border="0" />
+									</a>
+								</td>
+								<td style="font-size: 0; line-height: 0;" width="20">&nbsp;</td>
+								<td>
+									<a href="http://www.facebook.com/">
+										<img src="images/fb.png" alt="Facebook." width="38" height="38" style="display: block;" border="0" />
+									</a>
+								</td>
+							</tr>
+						</table>
+					</td>
+				</tr>
+			</table>
+		</td>
+	</tr>
+</table>
+
+			</td>
+		</tr>
+	</table>
+</body>
+</html>
